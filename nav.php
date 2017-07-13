684a4f2668807c9cb03321e32036a39562e94c74
Robert Grober <rgrober@gmail.com>
2017-07-12 20:49:31 -0600

    Responsive overlay nnav successfully implemented.

--- nav.php
+++ nav.php
@@ -4,19 +4,24 @@
 
 <aside>
   <nav>
-  	<div class="navSubAd">
-  		<button class="buttonSecondary">Subscribe</button>
-  		<button class="buttonMain">Advertise with us</button>
-  	</div>
-  	<img class="bhWeekly_logo_nav" src="/img/logo/bhWeekly_large.png">
-   <div class="navItems">
-	   	<div class="navItemsHolder">
-	   		<ul>
-		   	      <li><a class='navLinkOverlay' href="/thisweek.php">This Week</a></li>
-		   	      <li><a class='navLinkOverlay' href="/staff.php">Staff</a></li>
-		   	      <li><a class='navLinkOverlay' href="/pastissues.php">Past Issues</a></li>
-		   	      <li><a class='navLinkOverlay' href="/contact.php">Contact</a></li>
-		   	    </ul>
+	  	<div class="navContainer">
+	  		<div class="navSubAd">
+	  			<button class="buttonSecondary">Subscribe</button>
+	  		  	<button class="buttonMain">Advertise with us</button>
+		  	</div>
+		  	<div class="logoContainer">
+		  		<img class="bhWeekly_logo_nav" src="/img/logo/bhWeekly_large.png">
+		  		<img class="bhWeekly_logo_nav_horizontal" src="/img/logo/bhWeekly_horizontal.png">
+		  	</div>
+		   <div class="navItems">
+			   	<div class="navItemsHolder">
+			   		<ul>
+			   	      <li><a class='navLinkOverlay' href="/thisweek.php">This Week</a></li>
+			   	      <li><a class='navLinkOverlay' href="/staff.php">Staff</a></li>
+			   	      <li><a class='navLinkOverlay' href="/pastissues.php">Past Issues</a></li>
+			   	      <li><a class='navLinkOverlay' href="/contact.php">Contact</a></li>
+			   	    </ul>
+				</div>
 		   	</div>
    	</div>
    	<button class="outer-close toggle-overlay">
